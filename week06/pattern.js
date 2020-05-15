8a9c39d0582a5c452cb03fdd2740ada6c432eeeb
PingChuanXi <290944015@qq.com>
2020-05-15 11:36:18 +0800

    Update pattern.js

--- week06/pattern.js
+++ week06/pattern.js
@@ -1,33 +1,32 @@
-const assert = require('assert').strict;
-
-let start;
-
 module.exports = function match(pattern, string) {
-  const states = pattern.split('').map((p, idx) => generate(p, idx, pattern.length))
-  let idx = 0;
-  let matchIdx = 0;
-  start = states[idx];
-  for (let i = 0; i < string.length; i++) {
-    const c = string[i];
-    idx = states[idx](c);
-    if (idx === -1) {
-      return true;
-    } else if (idx > 0 && matchIdx === 0) {
-      matchIdx = i;
-    } else if (idx === 0 && matchIdx > 0) {
-      i = matchIdx;
-      matchIdx = 0;
-    }
-  }
-  return false;
+  return strStr(string,pattern) === 0;
 }
+
+var strStr = function(string, pattern) {
+    if(pattern === ''){
+        return 0
+    }
+    const states = pattern.split('').map((p, idx) => generate(p, idx, pattern.length))
+    let idx = 0;
+    let matchIdx = -1;
+    for (let i = 0; i < string.length; i++) {
+        idx = states[idx](string[i]);
+        if (idx === -1) {
+            return matchIdx===-1?0:matchIdx;
+        } else if (idx > 0 && matchIdx === -1) {
+            matchIdx = i;
+        } else if (idx === 0 && matchIdx > -1) {
+            i = matchIdx;
+            matchIdx = -1;
+        }
+    }
+    return -1;
+};
 
 function generate(p, idx, len) {
   return function (c) {
     if (p === c) {
       return (idx + 1 === len) ? -1 : (idx + 1);
-    } else if (idx !== 0) {
-      return start(c);
     }
     return 0;
   }
