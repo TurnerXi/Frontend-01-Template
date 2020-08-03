ea6a41c9b1d8a1b8d81c1198f17a834101c4aa10
TurnerXi <290944015@qq.com>
2020-08-04 00:20:12 +0800

    [feat]week17

--- week17/NOTE.md
+++ week17/NOTE.md
@@ -1,1 +1,9 @@
-# 每周总结可以写在这里
+# 组件化
+
+### Tab
+
+[https://turnerxi.github.io/Frontend-01-Template/week17/tab/index.html](https://turnerxi.github.io/Frontend-01-Template/week17/tab/index.html)
+
+### Template Tab
+
+[https://turnerxi.github.io/Frontend-01-Template/week17/tpl/index.html](https://turnerxi.github.io/Frontend-01-Template/week17/tpl/index.html)
---
+++ week17/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  mode: 'development',
+  entry: {
+    tab: './tab/script.js',
+    tpl: './tpl/script.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-transform-react-jsx', { pragma: 'create' }],
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: require.resolve('./css-loader.js'),
+      }
+    ]
+  },
+  optimization: {
+    minimize: false
+  }
+}
